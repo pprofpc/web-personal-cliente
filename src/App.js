fc23cfb9a29f7a18a5827679d546e27abf9be460
Pablo CANEPA <pprofpc@gmail.com>
2022-08-26 12:16:46 -0300

    Incorporando SASS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Web Persoanl - Cliente</h2>
+        <h2>Web Personal - <span>Cliente</span></h2>
       </header>
     </div>
   );
