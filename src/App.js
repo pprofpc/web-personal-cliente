f89350848e48eb2990b1bfb695a80878b67b9aaf
Pablo CANEPA <pprofpc@gmail.com>
2022-08-26 14:19:42 -0300

    Trabajando con Ant Deign

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import logo from './logo.svg';
+import 'moment/locale/es-mx';
+import locale from 'antd/es/date-picker/locale/es_ES';
+import { DatePicker, Space } from 'antd';
+
+import { Collapse } from 'antd';
+
 import './App.scss';
 
+const { Panel } = Collapse;
+
+const text = `
+  Este es el texto que puede aparecer dentro del Bloque Colapsable`
+
+
 function App() {
+
+  const onChange = (date, dateString) => {
+    console.log(date, dateString);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h2>Web Personal - <span>Cliente</span></h2>
+        <div>
+          <DatePicker locale={locale} onChange={onChange} format={"dd DD-MM-yy"} />
+        </div>
+
+        <Collapse defaultActiveKey={['1']} >
+          <Panel header="Información Complementaria" key="1">
+            <p>
+              <h3>¿Podré?</h3>
+              {text}</p>
+          </Panel>
+        </Collapse>
+
       </header>
     </div>
   );
