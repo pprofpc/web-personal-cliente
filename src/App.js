4c748e79afcbe40a2e75acf3d2c87a6e5fc03788
Pablo CANEPA <pprofpc@gmail.com>
2022-09-03 12:03:10 -0300

    Router Dom V6

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import 'moment/locale/es-mx';
 import locale from 'antd/es/date-picker/locale/es_ES';
 import { DatePicker, Space } from 'antd';
-
 import { Collapse } from 'antd';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 import './App.scss';
 
@@ -11,32 +12,62 @@ const { Panel } = Collapse;
 const text = `
   Este es el texto que puede aparecer dentro del Bloque Colapsable`
 
+const onChange = (date, dateString) => {
+  console.log(date, dateString);
+};
 
 function App() {
 
-  const onChange = (date, dateString) => {
-    console.log(date, dateString);
-  };
+
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Web Personal - <span>Cliente</span></h2>
-        <div>
-          <DatePicker locale={locale} onChange={onChange} format={"dd DD-MM-yy"} />
-        </div>
+    <Router>
+      <div className="App">
+        <h1>Sistema de Rutas Básico</h1>
 
-        <Collapse defaultActiveKey={['1']} >
-          <Panel header="Información Complementaria" key="1">
-            <p>
-              <h3>¿Podré?</h3>
-              {text}</p>
-          </Panel>
-        </Collapse>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route index element={<Home />} />
+          <Route exact path='/contact' element={<Contact />} />
+          <Route exact path='/elementos' element={<Elements />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
 
-      </header>
-    </div>
+      </div>
+    </Router>
   );
 }
+function Home() {
+  return <h2>Estamos en el componente Home</h2>;
+}
+
+function Contact() {
+  return (
+    <h2>Estamos en Contact...</h2>
+  );
+}
+
+function Elements() {
+  return (
+    <>
+      <h2>Web Personal - <span>Cliente</span></h2>
+      <div>
+        <DatePicker locale={locale} onChange={onChange} format={"dd DD-MM-yy"} />
+      </div>
+
+      <Collapse defaultActiveKey={['1']} >
+        <Panel header="Información Complementaria" key="1">
+          <p>
+            <h3>¿Podré?</h3>
+            {text}</p>
+        </Panel>
+      </Collapse>
+    </>
+  );
+}
+
+function Error404() {
+  return <h2>Página no encontrada :-(</h2>;
+};
 
 export default App;
