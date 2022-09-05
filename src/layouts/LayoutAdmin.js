d792ee727461ccc36f6e0e1944424943fb02a07f
Pablo CANEPA <pprofpc@gmail.com>
2022-09-04 21:06:14 -0300

    Previo de Layout

---
+++ src/layouts/LayoutAdmin.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Layout } from "antd";
+
+export default function LayoutAdmin() {
+    return (
+        <Layout>
+        <h2> Menu Sider</h2>
+        <div>Contenido...</div>
+        <h3>Footer...</h3>
+        </Layout>
+    );
+}
---
+++ src/layouts/LayoutBasic.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Layout } from "antd";
+
+export default function LayoutBasic() {
+    return (
+        <Layout>
+            <h2> Menu Sider</h2>
+            <div> Contenido....</div>
+            <h3>Footer...</h3>
+        
+        </Layout>
+    );
+}
